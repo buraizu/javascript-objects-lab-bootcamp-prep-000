c7b874fd2026b652edc20391c64aa6c1119f7ef8
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-07-28 18:31:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function deleteFromObjectByKey (object, key) {
 function deleteFromObjectByKey (object, key) {
   var newObj = Object.assign ({}, object);
   delete newObj[key];
+  return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey (object, key) {
